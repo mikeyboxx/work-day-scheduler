20144c065abea7fac9c78d11229cbd56d26b3cfc
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-05 23:33:14 -0400

    removed .vscode file

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// DOM Dependencies
+
+// User Interactions
+
+//  User navigates/refreshes to page
+//    - display current date in title
+//    - retrieve array of time blocks from local storage
+//    - generate time blocks that contain the hour, event text, and save button
+//    - mark the current hour red, past hour gray, future hours green
+//  User clicks on a time block
+//    - if time block is green, then make the time block text enterable
+//    - change the cursor type to be something else
+//
+//  User types in the time block
+//    
+//  User clicks on the save button
+//    - save the entered text in the two dimensional array
+//    - save to local storage
+//    - display 'save to local storage ⎷' under current date in title
+//
+// User hovers over save button 
+//    - change the pointer display
+//    
+
+// DATA
+// need to get current date, verbose
+// need to have an two-dimensional array of hours from 9am to 5pm, and event text
+// need to retrieve the two-dimensional srray from local storage
